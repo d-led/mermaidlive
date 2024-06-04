6a61c1f812b766584e675f289a8fab57dd561560
Dmitry Ledentsov <dmlled@yahoo.com>
2024-06-04 19:40:43 +0200

    more ui steps

--- src/steps/ui.steps.ts
+++ src/steps/ui.steps.ts
@@ -1,7 +1,14 @@
-import { Given, When , Then} from "@cucumber/cucumber";
+import { Given, When, Then } from "@cucumber/cucumber";
 import { ICustomWorld } from "../support/custom-world";
 import { config } from "../support/config";
 import { expect } from "@playwright/test";
+
+const slowExpect = expect.configure({ timeout: 10000 });
+
+const lastEventSeen = async function (page: any, event: string) {
+  const lastEvent = page.locator("#last-event");
+  await slowExpect(lastEvent).toContainText(event);
+};
 
 Given(
   "a system in state {string}",
@@ -9,53 +16,44 @@ Given(
     const page = this.page!;
     await page.goto(config.BASE_URL);
 
-    switch (state) {
-      case "waiting":
-        const waitingState = page.locator("[data-id=waiting]");
-        await expect.configure({ timeout: 10000 })(waitingState).toHaveClass(/\binProgress\b/);
-        break;
-      default:
-        throw "unknown state: " + state;
-    }
+    const stateNode = page.locator(`[data-id=${state}]`);
+    await slowExpect(stateNode).toHaveClass(/\binProgress\b/);
   },
 );
 
-When('the system {string} is requested', async function (command: string) {
-    const page = this.page!;
-    await page.getByRole('link', { name: command }).click();
+When("the system {string} is requested", async function (command: string) {
+  const page = this.page!;
+  await page.getByRole("link", { name: command }).click();
 });
 
-Then('work is completed', async function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then("work is completed", async function () {
+  await lastEventSeen(this.page!, "WorkDone");
 });
 
-Then('the request is ignored', async function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then("the request is ignored", async function () {
+  await lastEventSeen(this.page!, "RequestIgnored");
 });
 
-Then('the system is found in state {string}', async function (_state: string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then("the system is found in state {string}", async function (state: string) {
+  const page = this.page!;
+  const stateNode = page.locator(`[data-id=${state}]`);
+  await slowExpect(stateNode).toHaveClass(/\binProgress\b/);
 });
 
-When('some work has progressed', async function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+When("some work has progressed", async function () {
+  await lastEventSeen(this.page!, "Tick");
 });
 
-Then('work is canceled', async function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+Then("work is canceled", async function () {
+  await lastEventSeen(this.page!, "WorkAborted");
 });
 
-Given('two connected clients', async function () {
+Given("two connected clients", async function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return "pending";
 });
 
-Then('two clients have observed {string}', async function (_event: string) {
+Then("two clients have observed {string}", async function (_event: string) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return "pending";
 });
