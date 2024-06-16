7b3a200fc33063ce664075f9f0a7134a0f9a49dd
Dmitry Ledentsov <dmlled@yahoo.com>
2024-06-16 23:09:28 +0200

    replication now test-able in docker compose

--- traefik_peer_locator.go
+++ traefik_peer_locator.go
@@ -1,17 +1,102 @@
 package mermaidlive
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
+
 type TraefikPeerLocator struct {
 	traetraefikServicesUrl string
+	client                 *resty.Client
 }
 
 func NewTraefikPeerLocator(traetraefikServicesUrl string) *TraefikPeerLocator {
+	client := resty.New().
+		SetTimeout(15 * time.Second).
+		SetRetryCount(3).
+		SetRetryWaitTime(1 * time.Second)
+
 	return &TraefikPeerLocator{
 		traetraefikServicesUrl: traetraefikServicesUrl,
+		client:                 client,
 	}
 }
 
-func (*TraefikPeerLocator) GetPeers() ([]string, int, error) {
-	return []string{}, 1 /*this one*/, nil
+func (l *TraefikPeerLocator) GetPeers() ([]string, int, error) {
+	myIp, err := getMyIPv4()
+	if err != nil {
+		return nil, 1 /*this one*/, err
+	}
+	replicas := TraefikReplicas{}
+	resp, err := l.client.R().
+		SetResult(&replicas).
+		Get(l.traetraefikServicesUrl)
+
+	if err != nil {
+		return nil, 1 /*this one*/, err
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		return nil, 1 /*this one*/, fmt.Errorf("trafik returned [%d]: %s", resp.StatusCode(), resp.String())
+	}
+
+	res := []string{}
+	for server, status := range replicas.ServerStatus {
+		ip, err := getIPOf(server)
+		if err != nil {
+			log.Printf("Could not extract IP from '%s'", server)
+		}
+		if strings.ToLower(status) != "up" {
+			// no need to talk to replicas that are not up
+			continue
+		}
+		if ip != myIp {
+			res = append(res, ip)
+		}
+	}
+
+	return res, len(replicas.LoadBalancer.Servers), nil
+}
+
+func getIPOf(replicaUrl string) (string, error) {
+	u, err := url.Parse(replicaUrl)
+	if err != nil {
+		return "", err
+	}
+	if u.Host == "" {
+		return "", err
+	}
+	ip, _, err := net.SplitHostPort(u.Host)
+	if err != nil {
+		return "", err
+	}
+	return ip, nil
+}
+
+func getMyIPv4() (string, error) {
+	myHostname, err := os.Hostname()
+	if err != nil {
+		return "", err
+	}
+	ips, err := net.LookupIP(myHostname)
+	if err != nil {
+		return "", err
+	}
+	for _, ip := range ips {
+		if ipv4 := ip.To4(); ipv4 != nil {
+			return ipv4.String(), nil
+		}
+	}
+	return "", errors.New("could not find my IPv4")
 }
 
 // via https://app.quicktype.io/
