fb27c1f4a3d6b29853e34d5152ecd2a4bd854d5e
Dmitry Ledentsov <dmlled@yahoo.com>
2024-06-17 19:08:42 +0200

    remove migration logic after all replicas up to date

--- cmd/mermaidlive/main.go
+++ cmd/mermaidlive/main.go
@@ -20,8 +20,6 @@ const pubSubChannelCapacity = 1024
 const defaultCountdownDelay = 800 * time.Millisecond
 
 func main() {
-	migrate()
-
 	flag.Parse()
 
 	if *transpileOnly {
@@ -71,37 +69,4 @@ func getCountdownDelay() time.Duration {
 	}
 	log.Printf("countdown delay: %v", d)
 	return d
-}
-
-func migrate() {
-	oldFilename := os.Getenv("OLD_COUNTER_FILENAME")
-	newFilename := os.Getenv("COUNTER_FILENAME")
-	newDir := os.Getenv("COUNTER_DIRECTORY")
-	if oldFilename == "" || newFilename == "" || newDir == "" {
-		log.Println("nothing to migrate!")
-		return
-	}
-	err := os.MkdirAll(newDir, os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-	err = copyFile(oldFilename, newFilename)
-	if err != nil {
-		log.Println("original counter file already copied")
-		return
-	}
-	log.Println("copied counter file")
-	err = os.Remove(oldFilename)
-	if err != nil {
-		panic(err)
-	}
-	log.Println("migrations complete")
-}
-
-func copyFile(src, dst string) error {
-	text, err := os.ReadFile(src)
-	if err != nil {
-		return err
-	}
-	return os.WriteFile(dst, text, 0644)
 }
